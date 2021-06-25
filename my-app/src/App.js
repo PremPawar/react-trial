502118e70f15d2f8586f4f6cfe04a4137cbdcc89
Prem <prempawar28@gmail.com>
2021-06-25 23:00:35 +0530

    final nav

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,39 @@
 import React from 'react';
-import { 
-  BrowserRouter as Router, 
-  Switch, 
+import {
+  BrowserRouter as Router,
   Route,
-  Redirect 
+  Redirect,
+  Switch
 } from 'react-router-dom';
 
 import About from './pages/about/about';
 import Contact from './pages/contact/contact';
 import Home from './pages/home/home';
 import Services from './pages/services/services';
+import Navbar from './Components/Navbar/Navbar';
 
-const App = () =>  {
+const App = () => {
   return (
-    <Router>
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Home/>
-          </Route>
-
-          <Route path="/about" exact>
-            <About/>
-          </Route>
-
-          <Route path="/contact" exact>
-            <Contact/>
-          </Route>
-
-          <Route path="/services" exact>
-            <Services/>
-          </Route>
-
-          <Redirect to="/"  />
-        </Switch>
-      </main>
-
-    </Router>
+   <Router>
+    <Navbar/>
+    <main>
+      <Switch>
+        <Route path="/" exact>
+          <Home/>
+        </Route>
+        <Route path="/about" exact>
+          <About/>
+        </Route>
+        <Route path="/services" exact>
+          <Services/>
+        </Route>
+        <Route path="/contact" exact>
+          <Contact/>
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </main>
+   </Router>
   );
 }
 
